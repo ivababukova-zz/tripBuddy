9823cdb252a47e93f7716398e1bc213af0626977
Kris Dimitrov <kriso.dimitrov@gmail.com>
2015-11-10 20:15:31 +0000

    navbar,create trip page, initial crate trip, some jquery'

---
+++ js/events.js
@@ -0,0 +1,19 @@
+/**
+ * Created by kriso on 10/11/15.
+ */
+
+$(document).ready(function(){
+    $("#createCarTrip").click(function(){
+        $("#createTripHome").hide();
+        $("#createCarTripPage").show();
+}
+    );
+
+
+
+    $("#uniqueTrip").click(function(){
+        console.log("hello");
+        });
+
+})
+
