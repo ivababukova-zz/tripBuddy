38b5da4a24778cd95b4877acebea1ee5ba9f47a5
ivababukova <ibabukova@gmail.com>
2015-11-11 22:26:58 +0000

    Login page layout is done. Moved login to index file. Trying to figure out how to set css display propery using jquery.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
+/**
+ * Created by iva on 11/11/15.
+ */
+
+function hideElement(id) {
+  document.getElementById(id).style.display = 'none';
+  if(id === "startPage") {
+    showElement('loginPage');
+  }
+}
+
+function showElement(id) {
+  document.getElementById(id).style.display = 'block';
+}
+
+// this is for log in
+function getLocalProfile(id){
+  console.log("zdrasti");
+  //var profileImgSrc      = localStorage.getItem("pictures/navbar/userAvatar.png");
+  //var profileName        = "TestUser";
+
+  $("#startPage").css({"display":"none"});
+  hideElement(id);
+  hideElement('sth');
+  showElement('navbar');
+  showElement('createTripHome');
+}
 
 
-function ivaSfunction(){
-  document.getElementById("login").innerHTML="Hello Iva and Kris!";
+var display = 'block';
+
+function setDisplay() {
+  return 'block';
 }
