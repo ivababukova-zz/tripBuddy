a25afe035542351f2c2ba4c9b09c76349dfbe3c2
ivababukova <ibabukova@gmail.com>
2015-11-12 20:39:40 +0000

    made something hacky to get the pages load in the correct order

--- js/main.js
+++ js/main.js
@@ -1,34 +1,50 @@
 /**
  * Created by iva on 11/11/15.
  */
+var kupa = {};
 
-function hideElement(id) {
-  document.getElementById(id).style.display = 'none';
-  if(id === "startPage") {
-    showElement('loginPage');
+$( document ).ready(function() {
+    preload();
+});
+
+
+function preload(){
+  console.log(kupa.kupa);
+
+  if(kupa.kupa === undefined) {
+    console.log("in preload func");
+    $("#startPage").css({"display": 'block'});
+    $("#loginPage").css({"display": 'none'});
+    $("#navbar").css({"display": 'none'});
+    $("#createTripHome").css({"display": 'none'});
+    $('#body').css({"onload": 'foo()'});
+    kupa.kupa = "kupa";
   }
 }
 
-function showElement(id) {
-  document.getElementById(id).style.display = 'block';
-}
-
 // this is for log in
-function getLocalProfile(id){
+function getLocalProfile(){
   console.log("zdrasti");
   //var profileImgSrc      = localStorage.getItem("pictures/navbar/userAvatar.png");
   //var profileName        = "TestUser";
 
-  $("#startPage").css({"display":"none"});
-  hideElement(id);
-  hideElement('sth');
-  showElement('navbar');
-  showElement('createTripHome');
+  $("#startPage").css({"display":'none'});
+  $("#loginPage").css({"display":'block'});
+  $("#navbar").css({"display":'block'});
+  $("#createTripHome").css({"display":'block'});
+
 }
 
+function login(){
+  kupa = {};
+  console.log("*****");
+  $("#sth").css({"display":'none'});
+  $("#loginPage").css({"display":'none'});
+  $("#navbar").css({"display":'block'});
+  $("#createTripHome").css({"display":'block'});
+  isLoggedIn = true;
+}
 
-var display = 'block';
-
-function setDisplay() {
-  return 'block';
+function foo(){
+  console.log("in function foo");
 }
