61ce2b33cabbc04b1165666d747ba5907e18793b
Kris Dimitrov <kriso.dimitrov@gmail.com>
2015-11-22 18:01:01 +0000

    create car trip page done, google map added functionality to be implemented later

--- js/createTrip.js
+++ js/createTrip.js
@@ -6,7 +6,8 @@ var carModels= ["Ford Fiesta","Ford Focus","Vauxhall Corsa","Volkswagen Golf","V
     "Fiat 500", "Nisan Juke","Mercedes C-Class"];
 
 var times = ["00:00","00:30","01:00","01:30","02:00","02:30","03:00","03:30","04:00","04:30","05:00","05:30","06:00","06:30","07:00","07:30","08:00","08:30","09:00","09:30",
-             "10:00","10:30","11:00","11:30","12:00","12:30","13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:30", "18:00", "18:30", "18:00", "18:30", "18:00", "18:30", "18:00", "18:30", "18:00", "18:30", "18:00", "18:30", "18:00", "18:30", "18:00"]
+             "10:00","10:30","11:00","11:30","12:00","12:30","13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:30", "18:00", "18:30", "19:00", "19:30",
+             "20:00", "20:30", "21:00", "21:30", "22:00", "22:30", "23:00", "23:30"];
 
 $(document).ready(function() {
         $("#createCarTrip").click(function(){
@@ -26,13 +27,26 @@ $(document).ready(function() {
             $("#dateAndTime").hide();
             $("#weekDays").show();
         });
+        function initialize() {
+            var mapProp = {
+                center:new google.maps.LatLng(51.508742,-0.120850),
+                zoom:5,
+                mapTypeId:google.maps.MapTypeId.ROADMAP
+            };
+            var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+        }
+        google.maps.event.addDomListener(window, 'load', initialize);
 
     createTime();
+    addFreeSeats();
 }
 );
 
+/*
+add options to time drop down
+ */
 var createTime=function(e){
-    var timeDropDown = document.getElementById("timeOptions")
+    var timeDropDown = document.getElementById("timeOptions");
     var timeArrayLength = times.length;
     var htmlString = "";
     for( var i=0; i< timeArrayLength; i++){
@@ -40,4 +54,13 @@ var createTime=function(e){
        htmlString = htmlString.concat(" <li><a href=\"#\">",times[i],"</a></li>")
     }
     timeDropDown.innerHTML = htmlString;
-}
+};
+
+var addFreeSeats = function(e){
+    var seatDropDown = document.getElementById("freeSeats");
+    var htmlString = "";
+    for (var j=1;j<6;j++){
+        htmlString =htmlString.concat(" <li><a href=\"#\">",j,"</a></li>");
+    }
+    seatDropDown.innerHTML=htmlString;
+};
