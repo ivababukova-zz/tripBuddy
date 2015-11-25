efe8c265fdb8f4bf6ab671bae4bc97834694fbe7
Kris Dimitrov <kriso.dimitrov@gmail.com>
2015-11-25 14:31:48 +0000

    sound added to current trips

--- js/currentTripsEvents.js
+++ js/currentTripsEvents.js
@@ -5,6 +5,9 @@
 
 $(document).ready(function(){
   $("#carTripsButton").click(function(){
+    var sound = new Audio("sounds/car.wav");
+    sound.currentTime=0;
+    sound.play();
 
     $("#TaxiTrips").hide();
     $("#walkTrips").hide();
@@ -13,18 +16,27 @@ $(document).ready(function(){
   });
 
   $("#taxiTripsButton").click(function(){
+    var sound = new Audio("sounds/taxi.mp3");
+    sound.currentTime=0;
+    sound.play();
     $("#TaxiTrips").show();
     $("#walkTrips").hide();
     $("#carTrips").hide();
   });
 
   $("#walkTripsButton").click(function(){
+    var sound = new Audio("sounds/walk.mp3");
+    sound.currentTime=0;
+    sound.play();
     $("#TaxiTrips").hide();
     $("#walkTrips").show();
     $("#carTrips").hide();
   });
 
   $("#allTripButton").click(function(){
+      var sound = new Audio("sounds/all.wav");
+      sound.currentTime=0;
+      sound.play();
     $("#TaxiTrips").show();
     $("#walkTrips").show();
     $("#carTrips").show();
--- js/events.js
+++ js/events.js
@@ -3,18 +3,9 @@
  */
 
 $(document).ready(function(){
-    $("#createCarTrip").click(function(){
-        $("#createTripHome").hide();
-        $("#createCarTripPage").show();
-}
-    );
 
-  /*  $("#loginButton").click(function(){
-        $("#createTripHome").show();
-        $("#navbar").show();
-        $("#mainpage").hide();
-    });
-*/
+
+
     $('#datepicker').datepicker();
 
     $("#uniqueTrip").click(function(){
